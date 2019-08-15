997934a9faf7ab58cc907b3719f181b9ef095bb3
Sebastian Benjamin <sebastiancbenjamin@gmail.com>
2019-08-15 15:31:52 -0700

    Change contact dob to a date rather than datetime

--- Deq.Demo.Contact.Web/Models/Contact.cs
+++ Deq.Demo.Contact.Web/Models/Contact.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Deq.Demo.Contact.Web.Models
 {
@@ -8,6 +9,7 @@ namespace Deq.Demo.Contact.Web.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string ContactNumber { get; set; }
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}")]
         public DateTime DateOfBirth { get; set; }
         public string DepartmentId { get; set; }
         public string DepartmentName { get; set; }
